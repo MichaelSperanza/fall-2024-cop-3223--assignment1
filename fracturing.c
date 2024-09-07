bdc730a46def624727084fc66613d10b1e66f38c
MichaelSperanza <speranzam2003@gmail.com>
2024-09-07 18:21:50 -0400

    Add files via upload

---
+++ fracturing.c
@@ -0,0 +1,35 @@
+//*******************************
+// assignment1.c
+// Author: Michael Speranza
+// Class: COP 3223, Professor Parra
+// Purpose: 
+// Input: None
+//
+// Output: 
+// //******************************
+
+//Header file
+
+#include <stdio.h>
+#include <math.h>
+
+double calculateDistance()
+{
+    double x1;
+    double x2;
+    printf("Enter x1 and x2: \n");
+    scanf("%lf, %lf", &x1, &x2);
+    
+    double y1;
+    double y2;
+    printf("Enter y1 and y2: \n");
+    scanf("%lf, %lf", &y1, &y2);
+
+}
+
+int main(int argc, char **argv)
+{
+    calculateDistance();
+
+    return 0;
+}
