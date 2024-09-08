6c9860d00c4086db8b86cd3641abd9b07a46cbad
MichaelSperanza <speranzam2003@gmail.com>
2024-09-08 04:16:25 +0000

    fracturing.c

--- fracturing.c
+++ fracturing.c
@@ -14,20 +14,23 @@
 #include <math.h>
 #define pi 3.14159
 
+// Distance Function
 double calculateDistance()
 {
+    // Asking for user input
     double x1;
     double x2;
     
     printf("Enter x1 and x2: \n");
-    scanf("%lf, %lf", &x1, &x2);
+    scanf("%lf %lf", &x1, &x2);
     
     double y1;
     double y2;
     
     printf("Enter y1 and y2: \n");
-    scanf("%lf, %lf", &y1, &y2);
+    scanf("%lf %lf", &y1, &y2);
 
+    // Math
     double change_x;
     change_x = x2 - x1;
     
@@ -46,49 +49,161 @@ double calculateDistance()
     double Distance;
     Distance = sqrt(Dsqrd);
 
+    // Print and return distance
     printf("The distance between the two points is %.3lf \n", Distance);
 
     return Distance;
 
 }
 
-double calculatePerimeter(double Distance)
+// Perimeter function
+double calculatePerimeter()
 {
+    // Calling the Distance function
+    double Distance = calculateDistance();
+    double Perimeter;
+
+    // Math
+    Perimeter = Distance * pi;
+
+    // Print perimeter and return difficulty
+    double PDifficulty;
+    PDifficulty = 4;
+    printf("The perimeter of the city encompassed by your request is %.3lf \n", Perimeter);
     
+    return PDifficulty;
+   
+}
+
+// Area Function
+double calculateArea()
+{
+    // Calling the Distance Function
+    double Distance = calculateDistance();
+
+    // Math
+    double radius;
+    radius = Distance / 2;
+
+    double Area;
+    Area = pi * pow(radius, 2);
+
+    // Print the area and return Difficulty
+    double ADifficulty;
+    ADifficulty = 3;
+    printf("The area of the city encompassed by your request is %.3lf \n", Area);
+
+    return ADifficulty;
+
+}
+
+// Width Function
+double calculateWidth()
+{
+    // Asking for user input
     double x1;
     double x2;
     
     printf("Enter x1 and x2: \n");
-    scanf("%lf, %lf", &x1, &x2);
+    scanf("%lf %lf", &x1, &x2);
     
     double y1;
     double y2;
     
     printf("Enter y1 and y2: \n");
-    scanf("%lf, %lf", &y1, &y2);
+    scanf("%lf %lf", &y1, &y2);
 
-    double Perimeter;
-    Perimeter = Distance * pi;
-
+    // Math
+    double change_x;
+    change_x = x2 - x1;
     
+    double change_y;
+    change_y = y2 - y1;
 
+    double Xsqrd;
+    Xsqrd = change_x * change_x;
+    
+    double Ysqrd;
+    Ysqrd = change_y * change_y;
 
+    double Wsqrd;
+    Wsqrd = Xsqrd + Ysqrd;
+    
+    double Width;
+    Width = sqrt(Wsqrd);
 
-   
+    // Print and return statements
+    double WDifficulty;
+    WDifficulty = 1;
+    printf("The width of the city encompassed by your request is %.3lf \n", Width);
 
+    return WDifficulty;
 
+}
 
+// Height function
+double calculateHeight()
+{
+    // Asking for user input
+    double x1;
+    double x2;
+    
+    printf("Enter x1 and x2: \n");
+    scanf("%lf %lf", &x1, &x2);
+    
+    double y1;
+    double y2;
+    
+    printf("Enter y1 and y2: \n");
+    scanf("%lf %lf", &y1, &y2);
 
+    // Math
+    double change_x;
+    change_x = x2 - x1;
+    
+    double change_y;
+    change_y = y2 - y1;
 
+    double Xsqrd;
+    Xsqrd = change_x * change_x;
+    
+    double Ysqrd;
+    Ysqrd = change_y * change_y;
 
+    double Hsqrd;
+    Hsqrd = Xsqrd + Ysqrd;
+    
+    double Height;
+    Height = sqrt(Hsqrd);
 
+    // Print and difficulty statements
+    double HDifficulty;
+    HDifficulty = 1;
+    printf("The height of the city encompassed by your request is %.3lf \n", Height);
+
+    return HDifficulty;
+
+}
+
+// UserInput function
+double askForUserInput()
+{
+    double input;
+    scanf("%lf", &input);
+
+    // Return the input value
+    return input;
 }
 
 int main(int argc, char **argv)
 {
     calculateDistance();
     calculatePerimeter();
-    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
+    askForUserInput();
+
 
     return 0;
 }
